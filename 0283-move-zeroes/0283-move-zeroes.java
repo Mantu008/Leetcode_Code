35d94c7daf8831b2df0272f119e85b9315532e0d
MANTU KUMAR MORYA <128251996+Mantu008@users.noreply.github.com>
2023-09-08 15:47:17 +0530

    Time: 128 ms (5.02%), Space: 46.1 MB (5.4%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -7,17 +7,17 @@ class Solution {
     }
 
      public static void remove(int arr[]){
-          int j=0,k=0;
+          int j=0;
           for(int i=0;i<arr.length;i++){
                if(arr[i]==0){
-                   k++;
                    continue;
                }else{
                   arr[j]=arr[i];
                   j++;
                }
           }   
-          for(int i=arr.length-k;i<arr.length;i++){
+
+          for(int i=j;i<arr.length;i++){
                       arr[i]=0;
           }
 }
