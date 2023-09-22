2accd3dbeddbd911133b9768bf60010137c98f23
MANTU KUMAR MORYA <128251996+Mantu008@users.noreply.github.com>
2023-09-22 23:23:50 +0530

    Time: 1 ms (99.95%), Space: 44.7 MB (24.64%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+          int n=nums.length;
+          int ans=0;
+          for(int i=0;i<n;i++){
+              ans^=nums[i];
+          }   
+          return ans;
+    }
+}
