6da50202de4136a936cbfa1226bbe8f6269b79b9
MANTU KUMAR MORYA <128251996+Mantu008@users.noreply.github.com>
2023-09-23 17:24:34 +0530

    Time: 5 ms (90.55%), Space: 43.7 MB (97.44%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,59 +1,19 @@
 class Solution {
     public int firstUniqChar(String s) {
 
-    //     Queue<Character> q = new LinkedList<>();
-    //     String str=s;
-    //     String str1="";
-    //     int p=-1;
 
-    //     for(int i=0;i<str.length();i++){
-    //            q.offer(str.charAt(i));
-    //     }
-
-    //   while(!q.isEmpty()){
-    //       Character c=q.poll(); 
-    //         if(q.contains(c)){
-    //             while(q.contains(c)){
-    //                    q.remove(c);
-    //             }
-    //         }else{
-    //             str1=str1+c;
-    //             break;
-    //         }
-    //   }
-
- 
-
-    //      for(int i=0;i<str.length();i++){
-    //         if(str1.length()==1){
-    //             if(str.charAt(i)==str1.charAt(0)){
-    //                    System.out.println("cherector:"+str.charAt(i));
-    //                    System.out.println("index:-"+i);
-    //                    p=i;
-    //                    break;
-    //             }
-    //         }
-    //      }
-
-    //      if(p==-1){
-    //           return -1;
-    //      }else{
-    //          return p;
-    //      }
-
-
-
-
-        int[] arr = new int[26];
-        for(int i=0;i<s.length();i++){
-           arr[s.charAt(i)-'a']++;
+        int[] arr = new int[26];        //creat array of length of 26
+        for(int i=0;i<s.length();i++){     //store each charector frequency
+           arr[s.charAt(i)-'a']++;            
         }
 
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<s.length();i++){    //check which first charactor frequency is one
               if(arr[s.charAt(i)-'a']==1){
                     return i;
               } 
-        }
+        }   
+
+     //after traverce each loop here we not found any unique char so return -1  
 
         return -1;
        
@@ -62,13 +22,3 @@ class Solution {
         
     }
 }
-
-
-//  for (int i = 0; i < s.length(); i++) {
-//             arr[s.charAt(i) - 'a']++;
-//         }
-
-//         for (int i = 0; i < s.length(); i++) {
-//             if (arr[s.charAt(i) - 'a'] == 1) return i;
-//         }
-//         return -1;
