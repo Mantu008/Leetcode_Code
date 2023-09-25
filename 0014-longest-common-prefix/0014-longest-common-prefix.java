b7919ea0467ed1a8a5023d26566f1bd2f232d957
MANTU KUMAR MORYA <128251996+Mantu008@users.noreply.github.com>
2023-09-25 22:54:08 +0530

    Time: 2 ms (28.53%), Space: 40.5 MB (40.67%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,22 +4,21 @@ class Solution {
      for(int i=1;i<strs.length;i++){
          res=common(res,strs[i]);
      }
-
      return res;
 
     }
 
 public static String common(String s1,String s2){
-    String res="";
+    StringBuffer res=new StringBuffer();
     int n=s1.length()<s2.length()?s1.length():s2.length();
     for(int i=0;i<n;i++){
          if(s1.charAt(i)==s2.charAt(i)){
-                 res=res+s1.charAt(i);
+                 res.append(s1.charAt(i));
          }else{
             break;
          }
     }
-      return res;
+      return res.toString();
 }
 
 }
