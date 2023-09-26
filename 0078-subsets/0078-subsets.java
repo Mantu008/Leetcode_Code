757d8b4136d51b79c14452011c18dc6b1623f3e7
MANTU KUMAR MORYA <128251996+Mantu008@users.noreply.github.com>
2023-09-26 22:50:21 +0530

    Time: 0 ms (100%), Space: 42 MB (26.55%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        int n=nums.length;
+        ArrayList<Integer>list=new ArrayList<>();
+        List<List<Integer>> resultList = new ArrayList<>(); 
+        int idx=0;
+        Printsubn(idx,nums,n,list,resultList);
+        return resultList;
+
+    }
+
+      public static void Printsubn(int idx,int arr[],int n,ArrayList<Integer>list, List<List<Integer>> resultList){
+         if(idx>=n){
+                resultList.add(new ArrayList<>(list));
+               return;
+         }
+         list.add(arr[idx]);
+         Printsubn(idx+1,arr,n,list,resultList);
+         int size=list.size()-1;
+         list.remove(size);
+         Printsubn(idx+1,arr,n,list,resultList);
+         return;
+    }
+}
