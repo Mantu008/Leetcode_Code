e9785852e9876cc347a85a6b01b897a2294b12ef
MANTU KUMAR MORYA <128251996+Mantu008@users.noreply.github.com>
2023-09-30 00:15:20 +0530

    Time: 557 ms (10.91%), Space: 44 MB (61.77%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+
+         ArrayList<Integer>list=new ArrayList<>();
+        List<List<Integer>> resultList = new ArrayList<>(); 
+        boolean freq[]=new boolean[nums.length];
+        Printprmution(nums,list,resultList,freq);
+
+        return resultList;
+        
+    }
+
+     public static void Printprmution(int arr[],ArrayList<Integer>list, List<List<Integer>> resultList,boolean []freq){
+         if(list.size()==arr.length){
+                if(!resultList.contains(list)){
+                        resultList.add(new ArrayList<>(list));
+                }
+               return;
+         }
+        
+           for(int i=0;i<arr.length;i++){
+                if(!freq[i]){
+                     freq[i]=true;
+                     list.add(arr[i]);
+                     Printprmution(arr,list,resultList,freq);
+                     list.remove(list.size()-1);
+                     freq[i]=false;
+                }
+           }
+
+    }
+}
