d90fb855ebec75297704f212fddf1cb30ab31bb0
MANTU KUMAR MORYA <128251996+Mantu008@users.noreply.github.com>
2023-09-29 23:12:34 +0530

    Time: 2 ms (37.39%), Space: 44.1 MB (30%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -3,12 +3,11 @@ class Solution {
      ArrayList<Integer>list=new ArrayList<>();
         List<List<Integer>> resultList = new ArrayList<>(); 
         boolean freq[]=new boolean[nums.length];
-        int idx=0;
-        Printprmution(idx,nums,list,resultList,freq);
+        Printprmution(nums,list,resultList,freq);
         return resultList;
     }
 
-       public static void Printprmution(int idx,int arr[],ArrayList<Integer>list, List<List<Integer>> resultList,boolean []freq){
+       public static void Printprmution(int arr[],ArrayList<Integer>list, List<List<Integer>> resultList,boolean []freq){
          if(list.size()==arr.length){
                 resultList.add(new ArrayList<>(list));
                return;
@@ -18,7 +17,7 @@ class Solution {
                 if(!freq[i]){
                      freq[i]=true;
                      list.add(arr[i]);
-                     Printprmution(idx,arr,list,resultList,freq);
+                     Printprmution(arr,list,resultList,freq);
                      list.remove(list.size()-1);
                      freq[i]=false;
                 }
