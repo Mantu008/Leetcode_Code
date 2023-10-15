8a6c9c99361257a9a521b5003459bbe0d119bbc5
MANTU KUMAR MORYA <128251996+Mantu008@users.noreply.github.com>
2023-10-16 01:38:57 +0530

    Time: 0 ms (100%), Space: 42.8 MB (79.82%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -16,7 +16,7 @@
 class Solution {
      public boolean isBal=true;
     public boolean isBalanced(TreeNode root) {
-        isBalance(root);
+        int p=isBalance(root);
         return isBal;
     }
 
