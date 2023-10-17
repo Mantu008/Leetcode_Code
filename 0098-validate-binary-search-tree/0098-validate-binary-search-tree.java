88820d2a43e4ed617a34cf257de6cbda88242e66
MANTU KUMAR MORYA <128251996+Mantu008@users.noreply.github.com>
2023-10-17 23:25:05 +0530

    Time: 0 ms (100%), Space: 43.7 MB (55.78%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -13,34 +13,57 @@
  *     }
  * }
  */
+
 class Solution {
     public boolean isValidBST(TreeNode root) {
-         BSTPair bp=isBST(root);
-         return bp.isBST;
+       return isBST(root,Long.MIN_VALUE,Long.MAX_VALUE);  
     }
 
-      public static class BSTPair{
-        boolean isBST;
-        long min;
-        long max;
-    }
 
-    public static  BSTPair isBST(TreeNode node){
-       if(node==null){
-          BSTPair bp=new BSTPair();
-          bp.min=(Long.MAX_VALUE);
-          bp.max=(Long.MIN_VALUE);
-          bp.isBST=true;
-          return bp; 
-        }
+  public static boolean isBST(TreeNode node,long min_val,long max_val){
 
-        BSTPair lp=isBST(node.left);
-        BSTPair rp=isBST(node.right);
-        BSTPair mp=new BSTPair();
-        mp.isBST=lp.isBST && rp.isBST && (node.val>lp.max && node.val<rp.min);
-        mp.min=Math.min(node.val,Math.min(lp.min, rp.min));
-        mp.max=Math.max(node.val,Math.max(lp.max, rp.max));
+     if(node==null){
+         return true;
+     }
 
-        return mp;
-    }
+     if(node.val>=max_val || node.val<=min_val){
+         return false;
+     }
+
+     return isBST(node.left,min_val,node.val) && isBST(node.right,node.val,max_val);
+
+  }
 }
+
+
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//          BSTPair bp=isBST(root);
+//          return bp.isBST;
+//     }
+
+//       public static class BSTPair{
+//         boolean isBST;
+//         long min;
+//         long max;
+//     }
+
+//     public static  BSTPair isBST(TreeNode node){
+//        if(node==null){
+//           BSTPair bp=new BSTPair();
+//           bp.min=(Long.MAX_VALUE);
+//           bp.max=(Long.MIN_VALUE);
+//           bp.isBST=true;
+//           return bp; 
+//         }
+
+//         BSTPair lp=isBST(node.left);
+//         BSTPair rp=isBST(node.right);
+//         BSTPair mp=new BSTPair();
+//         mp.isBST=lp.isBST && rp.isBST && (node.val>lp.max && node.val<rp.min);
+//         mp.min=Math.min(node.val,Math.min(lp.min, rp.min));
+//         mp.max=Math.max(node.val,Math.max(lp.max, rp.max));
+
+//         return mp;
+//     }
+// }
