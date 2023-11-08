5904440f203eeea807569f9fb503fd4724fd0c7b
MANTU KUMAR MORYA <128251996+Mantu008@users.noreply.github.com>
2023-11-08 12:20:51 +0530

    Time: 0 ms (100%), Space: 43.9 MB (96.01%) - LeetHub

--- 0792-binary-search/0792-binary-search.java
+++ 0792-binary-search/0792-binary-search.java
@@ -4,23 +4,21 @@ class Solution {
          return binarysearch(nums, target);
 
     }
+ public static int binarysearch(int arr[],int target){
+    int start=0;
+    int end=arr.length-1;
+    
+    while(start<=end){
+      int mid=start+(end-start)/2;
 
-    static int binarysearch(int[]arr,int target){
-
-        int start=0;
-        int end=arr.length-1;
-
-        while(start<=end){
-            int mid=start+(end-start)/2;
-
-            if(target>arr[mid]){
-                start=mid+1;
-            }else if(target<arr[mid]){
-                end=mid-1;  
-            }else{
-                return mid;
-            } 
-        }
-        return -1;
+      if(target>arr[mid]){
+         start=mid+1;
+      }else if(target<arr[mid]){
+          end=mid-1;
+      }else{
+        return mid;
+      }
     }
+    return -1;
+  }
 }
