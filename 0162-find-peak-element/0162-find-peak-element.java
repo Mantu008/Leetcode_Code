f4884609adbcf06fc61bc72ce3f4fccb184cc892
MANTU KUMAR MORYA <mantukumar87586299@gmail.com>
2024-07-04 20:32:49 +0530

    Time: 0 ms (100%), Space: 42.7 MB (8.78%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,19 +1,61 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int start = 0;
-        int end = nums.length - 1;    
-     
-        while (start < end) {
-            int mid = start + (end - start) / 2;   
-            if (nums[mid]<nums[mid+1]) {
-                start = mid+1;
-            } else {
-               end=mid;
-            }  
 
-          
+    if(nums.length==1) return 0;
+
+    for (int i = 0; i < nums.length; i++) {
+      if (i == 0) {
+        if (nums[i] > nums[i + 1]) {
+           return i;
         }
-
-        return end;
+      } else if (i == nums.length - 1) {
+        if (nums[i] > nums[i - 1]) {
+          return i;
+        }
+      } else if (nums[i] > nums[i + 1] && nums[i] > nums[i - 1]) {
+        return i;
+      }
     }
+
+    return 0;
+
+}
+
+ public static int searchPeak(int arr[]){
+
+   int start=0;
+   int end=arr.length-1;
+
+   if(arr.length==1) return 0;
+
+   while (start<=end) {
+      int mid=start+(end-start)/2;
+
+      if (mid > 0 && mid<arr.length-1) {
+        if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+         return mid;
+      }else if(arr[mid-1]>arr[mid]){
+          end=mid-1;
+      }else if(arr[mid+1]>arr[mid]){
+          start=mid+1;
+      }
+
+      }else if(mid==0){
+        if(arr[0]>arr[1]){
+            return 0;
+        }else{
+          return 1;
+        }
+      }else if(mid==arr.length-1){
+          if(arr[arr.length-1]>arr[arr.length-2]){
+                return arr.length-1;
+          }else{
+              return  arr.length-2; 
+          }
+      }
+      
+   }
+
+   return -1;
+ }
 }
