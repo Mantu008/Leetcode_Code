20e86c6d0f5f80a4f8bf98e7adca29ef34fa7c2d
MANTU KUMAR MORYA <mantukumar87586299@gmail.com>
2025-04-25 10:55:21 +0530

    Time: 0 ms (100%), Space: 44.3 MB (88.52%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,19 +11,18 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
 
-      ListNode fast=head;
-      ListNode slow=head;
-        
         while(fast!=null && fast.next!=null){
-            fast=fast.next.next;
             slow=slow.next;
-            if(fast==slow){
-                 return true;
+            fast=fast.next.next;
+
+            if(slow==fast){
+                return true;
             }
         }
 
         return false;
-
     }
 }
