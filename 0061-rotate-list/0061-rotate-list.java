2f4e969db5da2ce4e8b3ba544dad90524b82861b
MANTU KUMAR MORYA <mantukumar87586299@gmail.com>
2025-04-27 23:25:34 +0530

    Time: 0 ms (100%), Space: 42.6 MB (48.51%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,31 +10,37 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head==null|| head.next==null||k==0){
-               return head;
-        }
 
-        //find length of list
+       if(head==null || head.next==null || k==0){
+          return head;
+       }
 
-        ListNode temp=head; 
+        //find size and tail node
         int size=1;
-        while(temp.next!=null){
+        ListNode tail=head;
+        while(tail.next!=null){
             size++;
-            temp=temp.next;
+            tail=tail.next;
+        }
+        
+        //mark tail next to head
+        tail.next=head;
+        //find k in correcgt way in case if k is grator than list length
+        k= k % size;
+
+        //find new head position value and take a node and ittrate this to the new head position 
+        int newHeadPosition=size-k;
+        ListNode newTailNode=head;
+        for(int i=0;i<newHeadPosition-1;i++){
+            newTailNode=newTailNode.next;
         }
 
-       //point tail to head
-        temp.next=head;
-        k=k%size;
-        k=size-k;
-        while(k-->0){  
-         temp=temp.next;
-        }
-
-        //make new head and tail
-        head=temp.next;
-        temp.next=null;
+        //mark head
+        head=newTailNode.next;
+        //mark tail
+        newTailNode.next=null;
 
         return head;
+
     }
 }
