744cca09680021e3fa6447e2227b9197c0d5ae3d
MANTU KUMAR MORYA <mantukumar87586299@gmail.com>
2025-05-21 13:58:07 +0530

    Time: 4 ms (83.16%), Space: 68.9 MB (20.93%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,50 +9,44 @@
  * }
  */
 class Solution {
+    
     public boolean isPalindrome(ListNode head) {
-        //find size
-        ListNode temp=head;
-        int size=0;
-        while(temp!=null){
-           size++;
-           temp=temp.next;
-        }        
-
-        //find mid of linkedlist 
-        ListNode fast=head;
-        ListNode slow=head;
-        while(fast!=null && fast.next!=null){
-            fast=fast.next.next;
-            slow=slow.next;
-        }
-
-        //reverce half of list
-        ListNode prev=null;
-        ListNode curr=head;
-        while(curr!=slow){
-            ListNode next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
-        }
-
-        //check if list size id even or odd
-        if(size%2!=0){
-            slow=slow.next;
-        }
-
-        //at the end check first half to second half
-
-        while(slow!=null){
-            if(slow.val==prev.val){
-              slow=slow.next;
-              prev=prev.next;
-            }else{
-                return false;
-            }
-        }
-
-        return true;
-
+      return  IsPalindrome(head);
     }
+
+    boolean IsPalindrome(ListNode head) {
+            ListNode fast = head;
+            ListNode slow = head;
+            while (fast != null && fast.next != null) {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+
+            ListNode secondHalf = reverce(slow);
+            ListNode firstHalf = head;
+
+            while (secondHalf != null) {
+                if (firstHalf.val != secondHalf.val) {
+                    return false;
+                } else {
+                    firstHalf = firstHalf.next;
+                    secondHalf = secondHalf.next;
+                }
+            }
+
+            return true;
+
+        }
+
+        ListNode reverce(ListNode head) {
+            ListNode prev = null;
+            ListNode curr = head;
+            while (curr != null) {
+                ListNode next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+            }
+            return prev;
+        }
 }
