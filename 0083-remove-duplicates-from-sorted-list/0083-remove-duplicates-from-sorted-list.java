2c0c5048c46339ff2cd789c86a2edea5e033a54c
MANTU KUMAR MORYA <mantukumar87586299@gmail.com>
2025-05-21 11:21:55 +0530

    Time: 0 ms (100%), Space: 44.1 MB (72.79%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -13,12 +13,11 @@ class Solution {
         ListNode curr=head;
         while(curr!=null && curr.next!=null){
            if(curr.val==curr.next.val){
-               curr.next=curr.next.next;
+              curr.next=curr.next.next;
            }else{
               curr=curr.next;
            }
         }
-
         return head;
     }
 }
