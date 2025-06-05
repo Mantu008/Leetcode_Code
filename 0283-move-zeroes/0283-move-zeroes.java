a1876c1c1f730cea0524455ce487293dfd449bc9
MANTU KUMAR MORYA <mantukumar87586299@gmail.com>
2025-06-05 16:00:47 +0530

    Time: 2 ms (81.75%), Space: 45.8 MB (81.06%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,20 +1,19 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-         movAllZeroTOENd(nums);
+       moveZero(nums);
     }
 
-     public static void movAllZeroTOENd(int[] arr) {
-        int nonZeroIndex = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] != 0) {
-                arr[nonZeroIndex] = arr[i];
-                nonZeroIndex++;
-            }
+    public static void moveZero(int arr[]){
+        int nonZeroIdx=0;
+        for(int i=0;i<arr.length;i++){
+           if(arr[i]!=0){
+              arr[nonZeroIdx]=arr[i];
+              nonZeroIdx++;
+           }
         }
 
-        for (int i = nonZeroIndex; i < arr.length; i++) {
-            arr[i] = 0;
+        for(int i=nonZeroIdx;i<arr.length;i++){
+            arr[i]=0;
         }
     }
 }
