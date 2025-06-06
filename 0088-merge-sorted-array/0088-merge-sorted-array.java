6f0e0dfb48e2fa5820ca261aa73e3f43886be3f5
MANTU KUMAR MORYA <mantukumar87586299@gmail.com>
2025-06-06 15:57:40 +0530

    Time: 0 ms (100%), Space: 41.8 MB (98.08%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,40 +1,27 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int arr[]=new int[m+n];
-        sort(nums1,nums2,m,n,arr);
-    }
+       int p1=m-1;
+       int p2=n-1;
+       int p=m+n-1;
+       
+       while(p1>=0 && p2>=0){
+          if(nums1[p1]>nums2[p2]){
+             nums1[p]=nums1[p1];
+             p1--;
+          }else{
+             nums1[p]=nums2[p2];
+             p2--;
+          }
 
-     public static void sort(int nums1[],int nums2[],int m,int n,int arr[]){
-            int i=0,j=0,k=0;
-
-            while(i<m && j<n){
-                if(nums1[i]<=nums2[j]){
-                    arr[k]=nums1[i];
-                    i++;
-                    k++;
-                }else{
-                    arr[k]=nums2[j];
-                    j++;
-                    k++;
-                }
-            }
-
-            System.out.println(i+" "+j+" "+k);
-
-            if(i==m){
-                 for(int p=k;p<arr.length;p++){
-                    arr[p]=nums2[j];
-                    j++;
-                 }
-            }else{
-                for(int p=k;p<arr.length;p++){
-                      arr[p]=nums1[i];
-                      i++;
-                 }
-            }
-
-            for(int p=0;p<m+n;p++){
-            nums1[p]=arr[p];
+          p--;
        }
+
+      // Copy remaining nums2 elements if any
+
+      while(p2>=0){
+        nums1[p]=nums2[p2];
+        p2--;
+        p--;
+      }
     }
 }
