848e485ea34df55ff2d1602a54a70818e28c333a
MANTU KUMAR MORYA <mantukumar87586299@gmail.com>
2025-06-09 12:51:26 +0530

    Time: 2 ms (27.64%), Space: 57.5 MB (35.74%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,31 +1,48 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-      int arr[]=new int[nums.length];
-        rotate(nums,arr,k);
+        rotateByK(nums,k);
     }
+    public static void rotateByK(int arr[],int k){
+        //for large k
+        k=k%arr.length;
+        //if k is negative then make it corrosponding positive
+        if(k<0)  k=k+arr.length;
+        //if i is zero then simple not doing anything
+        if(k==0) return;
 
-      public static void rotate(int nums[],int arr[],int k){
-           int size=nums.length;
-            k=k%size;
-            k=size-k;
-              
-            int n=k;
-            int j=0;
-            for(int i=n;i<size;i++){
-                arr[j]=nums[i];
-                j++;    
-            }
+        //find mid to brake into two part
+        int mid=arr.length-k;
 
-            n=k;
+        //reverce first part of array
+        int start=0;
+        int end=mid-1;
+        reverce(arr,start,end);
 
-            for(int i=0;i<k;i++){
-                arr[j]=nums[i];
-                j++;
-            }
+        //reverce second part of array
+        start=mid;
+        end=arr.length-1;
+        reverce(arr,start,end);
 
-            for(int i=0;i<size;i++){
-                nums[i]=arr[i];
-          }
+        //reverce whole array for opt 
+        // a + b --> input
+        // a` + b`
+        // (a` + b`)`
+        // b + a  --> output
 
-     } 
+        start=0;
+        end=arr.length-1;
+        reverce(arr,start,end);
+
+
+    }
+    
+    public static void reverce(int arr[],int start,int end){
+         while(start<=end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+         }
+    }
 }
