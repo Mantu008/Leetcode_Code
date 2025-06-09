bd2ce2a3c23d17908e05ea64a03ae3d2f841ffe7
MANTU KUMAR MORYA <mantukumar87586299@gmail.com>
2025-06-09 10:15:09 +0530

    Time: 13 ms (6.32%), Space: 43.3 MB (62.64%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,47 +1,25 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        ArrayList<Integer> list = new ArrayList<>();
-        int m = nums1.length;
-        int n = nums2.length;
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        List<Integer> list = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
 
-        int i = 0;
-        int j = 0;
+        for(int num:nums1){
+            list.add(num);
+        }    
 
-        while (i < m && j < n) {
-
-            // Skip duplicates in nums1
-            while (i > 0 && i < m && nums1[i] == nums1[i - 1]) {
-                i++;
-            }
-
-            // Skip duplicates in nums2
-            while (j > 0 && j < n && nums2[j] == nums2[j - 1]) {
-                j++;
-            }
-
-            if (i >= m || j >= n) break;
-
-            if (nums1[i] < nums2[j]) {
-                i++;
-            } else if (nums1[i] > nums2[j]) {
-                j++;
-            } else {
-                list.add(nums1[i]);
-                i++;
-                j++;
+        for(int num:nums2){
+            if(list.contains(num) && !result.contains(num)){
+              result.add(num);
             }
         }
 
-        System.out.println(list);
-
-        int arr[] = new int[list.size()];
-
-        for (int p = 0; p < list.size(); p++) {
-            arr[p] = list.get(p);
+        int resultArr[]=new int[result.size()];
+        int i=0;
+        for(int num:result){
+           resultArr[i]=num; 
+           i++;
         }
 
-        return arr;
+        return resultArr;
     }
 }
